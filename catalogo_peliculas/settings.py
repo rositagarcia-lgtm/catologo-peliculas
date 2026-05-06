84c14280e72d1e7c9005e595fed26590b4f10c54
rositagarcia-lgtm <rosita.garcia@tecsup.edu.pe>
2026-05-06 12:21:47 -0500

    Update settings.py

--- catalogo_peliculas/settings.py
+++ catalogo_peliculas/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-+f7op_5nnbt7x5w%1@j@gqf5-5=uvr1af%_lqaa(q^+=kinajl
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
