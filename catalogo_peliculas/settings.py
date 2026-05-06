4840a041ad058eeecda4f4f057e48dedb36dda33
rositagarcia-lgtm <rosita.garcia@tecsup.edu.pe>
2026-05-06 12:14:38 -0500

    Update settings.py
    Base datos actualizada

--- catalogo_peliculas/settings.py
+++ catalogo_peliculas/settings.py
@@ -76,12 +76,8 @@ WSGI_APPLICATION = 'catalogo_peliculas.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'rositagarcia$peliculas', 
-        'USER': 'rositagarcia',           
-        'PASSWORD': 'tu_password_de_BD',  # La que creaste en la pestaña Databases
-        'HOST': '://pythonanywhere-services.com', # El host que te dan ahí
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
